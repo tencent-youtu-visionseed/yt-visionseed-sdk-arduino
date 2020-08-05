3bf48251dd339902d6e16cf1391a3e1d3278f739
chenliang <chenliang@tencent.com>
2020-08-05 12:08:03 +0800

    [IMV] Improved getResult() parameters

--- example-ext/example-ext.ino
+++ example-ext/example-ext.ino
@@ -32,38 +32,30 @@ void loop()
     const uint8_t *message = dl.recvRunOnce();
     if (message)
     {
-        uint8_t vs_path[3] = {VS_MODEL_FACE_DETECTION};
         uint32_t count = 0;
-        YtDataLink::getResult(message, &count, vs_path, 1);
+        YtDataLink::getResult(message, &count, {VS_MODEL_FACE_DETECTION});
 
         for (int i = 0; i < min(1, (int)count); i ++)
         {
             YtVisionSeedResultTypeRect rect;
-            YtVisionSeedResultTypeString faceName = {.conf = 0, .p = 0};
-            YtVisionSeedResultTypePoints points = {.count =0, .p = 0};
+            YtVisionSeedResultTypePoints points = {.count = 0, .p = 0};
             YtVisionSeedResultTypeArray pose = {.count =0, .p = 0};
 
-            // 获取检测框
-            vs_path[1] = i;
-            if (!YtDataLink::getResult(message, &rect, vs_path, 2))
+            // 获取检测框（模型ID路径：人脸检测/index）
+            if (!YtDataLink::getResult(message, &rect, {VS_MODEL_FACE_DETECTION, i}))
             {
                 continue;
             }
-            // 获取90点关键点
-            vs_path[2] = VS_MODEL_FACE_LANDMARK;
-            if (!YtDataLink::getResult(message, &points, vs_path, 3))
+            // 获取人脸姿态（模型ID路径：人脸检测/index/人脸姿态）
+            if (!YtDataLink::getResult(message, &pose, {VS_MODEL_FACE_DETECTION, i, VS_MODEL_FACE_POSE}))
             {
                 continue;
             }
-            // 获取人脸姿态
-            vs_path[2] = VS_MODEL_FACE_POSE;
-            if (!YtDataLink::getResult(message, &pose, vs_path, 3))
+            // 获取人脸关键点定位结果（模型ID路径：人脸检测/index/人脸配准）
+            if (!YtDataLink::getResult(message, &points, {VS_MODEL_FACE_DETECTION, i, VS_MODEL_FACE_LANDMARK}))
             {
                 continue;
             }
-            // 获取人脸识别结果
-            vs_path[2] = VS_MODEL_FACE_RECOGNITION;
-            YtDataLink::getResult(message, &faceName, vs_path, 3);
 
             float openess = 0;
             if (GetYtFaceShape(points, shape))
